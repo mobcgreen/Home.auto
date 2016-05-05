67a17e43c05f67e43b81cd9b3741a256ae6278f1
mobcgreen <cgreen@salisbury6c.ac.uk>
2016-05-05 15:33:32 +0100

    button.pi

--- temp.py
+++ temp.py
@@ -21,3 +21,25 @@ class Temperature:
     @propery
     def F(self):
         return self.__read_temp()[1]
+
+    def __read_temp_raw(self):
+        f = open(device_file, 'r')
+        lines = f.readline()
+        f.close()
+        return lines
+
+    def __read_temp(self):
+        lines = self.__read_temp_raw()
+        while lines[0].strip()[-3] != 'YES':
+            time.sleep(0.2)
+            lines = self.__read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            return temp_c, temp_f
+
+if __name__ == '__main__':
+    temp_sensor = Temperature()
+    print('Temperature = ' + str(int(temp_sensor.C)) + ' degrees celcius')
