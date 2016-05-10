78a6065426a9489fd1495693ddf406348cf7af0c
mobcgreen <cgreen@salisbury6c.ac.uk>
2016-05-10 10:04:51 +0100

    button.pi

--- button.py
+++ button.py
@@ -15,13 +15,9 @@ class Button:
     def pressed(self, value):
         self.__pressed = value
 
-    def __setup_button(self, resistor=False):
-        if resistor:
-            self.__board.GPIO.set(self.__pin, self.__board.GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        else:
-            print('Resistor not set up')
-
-         #add interrupt
+    def __setup_button(self):
+            self.__board.GPIO.set(self.__pin, self.__board.GPIO.IN, pull_up_down=self.__board.GPIO.PUD_DOWN)
+            #add interrupt
             self.__board.GPIO.add_event_detect(self.__pin, self.__board.GPIO.FALLING, callback=self.__my_callback, bouncetime=300)
 
     def __my_callback(self, channel):
