4c157c1c99c8ab50449899ec26344503c829439f
mobcgreen <cgreen@salisbury6c.ac.uk>
2016-05-10 09:21:16 +0100

    button.pi

--- button.py
+++ button.py
@@ -4,7 +4,7 @@ class Button:
     def __init__(self, gpio_object, pin):
         self.__board = gpio_object
         self.__pin = pin
-        self.__setup_button(22)
+        #self.__setup_button(22)
         self.__pressed = False
 
     @property
@@ -15,16 +15,16 @@ class Button:
     def pressed(self, value):
         self.__pressed = value
 
-    def __set_button(self, resistor=False):
-        if not resistor:
-            self.__board.GPIO.setup(self.__pin, self.__board.GPIO.IN)
-        elif resistor:
-            self.__board.GPIO.set(self.__pin, self.__board.GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        else:
-            print('Resistor not set up')
+    #def __set_button(self, resistor=False):
+     #   if not resistor:
+      #      self.__board.GPIO.setup(self.__pin, self.__board.GPIO.IN)
+       # elif resistor:
+        #    self.__board.GPIO.set(self.__pin, self.__board.GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        #else:
+         #   print('Resistor not set up')
 
         #add interrupt
-        self.__board.GPIO.add_event_detect(self.__pin, self.__board.GPIO.FALLING, callback=self.__my_callback, bouncetime=300)
+        #self.__board.GPIO.add_event_detect(self.__pin, self.__board.GPIO.FALLING, callback=self.__my_callback, bouncetime=300)
 
     def __my_callback(self, channel):
         self.pressed = True
