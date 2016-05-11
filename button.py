6653d0ff0d3e2a8defec2bb8414bb296f2e313ee
mobcgreen <cgreen@salisbury6c.ac.uk>
2016-05-11 11:25:35 +0100

    button.pi

--- button.py
+++ button.py
@@ -15,25 +15,30 @@ class Button:
     def pressed(self, value):
         self.__pressed = value
 
-    def __setup_button(self):
-            self.__board.GPIO.set(self.__pin, self.__board.GPIO.IN, pull_up_down=self.__board.GPIO.PUD_UP)
-            #add interrupt
-            self.__board.GPIO.add_event_detect(self.__pin, self.__board.GPIO.FALLING, callback=self.__my_callback, bouncetime=300)
+
+    def __setup_button(self, resistor=False):
+        if not resistor:
+            self.__board.GPIO.setup(self.__pin, self.__board.GPIO.IN)
+        elif resistor:
+            self.__board.GPIO.set(self.__pin, self.__board.GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        else:
+            print('Error setting up resistor')
+
+        # add interrupt
+        self.__board.GPIO.add_event_detect(self.__pin, self.__board.GPIO.FALLING, callback=self.__my_callback, bouncetime=300)
 
     def __my_callback(self, channel):
         self.pressed = True
 
-    if __name__ == "__main__":
-        from board import Board
-        from button import Button
-        rpi = Board()
-        button = Button(rpi, 22)
-
-        while True:
-            if button.pressed:
-                print('button pressed')
-                button.pressed = False
-        rpi.GPIO.cleanup()
 
 
-            
+if __name__ == "__main__":
+    from board import Board
+    rpi = Board()
+    button = Button(rpi, 22)
+
+    while True:
+        if button.pressed:
+            print('button pressed')
+            button.pressed = False
+    rpi.GPIO.cleanup()
