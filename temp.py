42cb51f1aa4dd7c182a3736e3a6afe93800c96ba
mobcgreen <cgreen@salisbury6c.ac.uk>
2016-05-12 14:25:51 +0100

    button.pi

--- temp.py
+++ temp.py
@@ -6,7 +6,7 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devces/'
-device_folder = glob.gpio(base_dir + '28*')[0]
+device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
 class Temperature:
