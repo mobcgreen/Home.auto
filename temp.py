4ea7130482cc50af37d195cb1b0f1c6467a8ee08
mobcgreen <cgreen@salisbury6c.ac.uk>
2016-05-12 14:27:57 +0100

    button.pi

--- temp.py
+++ temp.py
@@ -5,40 +5,41 @@ import time
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-base_dir = '/sys/bus/w1/devces/'
+base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-class Temperature:
 
-    def __int__(self):
+class Temperature:
+    def __init__(self):
         pass
 
     @property
     def C(self):
         return self.__read_temp()[0]
 
-    @propery
+    @property
     def F(self):
         return self.__read_temp()[1]
 
     def __read_temp_raw(self):
         f = open(device_file, 'r')
-        lines = f.readline()
+        lines = f.readlines()
         f.close()
         return lines
 
     def __read_temp(self):
         lines = self.__read_temp_raw()
-        while lines[0].strip()[-3] != 'YES':
+        while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
             lines = self.__read_temp_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
-            temp_string = lines[1][equals_pos+2:]
+            temp_string = lines[1][equals_pos + 2:]
             temp_c = float(temp_string) / 1000.0
             temp_f = temp_c * 9.0 / 5.0 + 32.0
             return temp_c, temp_f
+
 
 if __name__ == '__main__':
     temp_sensor = Temperature()
