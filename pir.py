8801281d7731d9c90cb9437c1be9966e0a432e29
cgreen <cgreen@salisbury6c.ac.uk>
2016-05-17 10:09:06 +0100

    pir.pi

--- pir.py
+++ pir.py
@@ -1,1 +1,35 @@
 
+class Pir:
+
+    def __init__(self, gpio_object, pin):
+        self.__board = gpio_object
+        self.__pirpin = pin
+        self.__setup_pir()
+
+    def __setup_pir(self):
+        self.__board.GPIO.setup(self.__pirpin, self.__board.GPIO.OUT)
+
+    def pir_motion(self):
+        print ('PIR Module Test(CTRL + C to exit)')
+        sleep(2)
+        print ('Ready')
+        while True:
+            if self.__board.GPIO.input(self.__pirpin):
+                print 'Motion Detected!'
+            sleep(1)
+
+
+if __name__ == '__main__':
+    from board import Board
+    from time import sleep
+    rpi = Board()
+    pir = Pir(rpi, 14)
+    pir.pir_motion()
+    rpi.GPIO.cleanup()
+
+
+
+
+
+
+
