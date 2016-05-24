c3ed330860ef8af8c20af38ff31bd18dd163ada4
cgreen <cgreen@salisbury6c.ac.uk>
2016-05-24 09:20:53 +0100

    pir.pi

---
+++ main.py
@@ -0,0 +1,19 @@
+from board import Board
+from led import Led
+from rgb import RGB_led
+from button import Button
+from buzzer import Buzzer
+from pir import Pir
+from temp import Temperature
+from time import sleep
+rpi = Board()
+
+led = Led(rpi, 13)
+led = RGB_led(rpi, 20, 21, 16)
+button = Button(rpi, 22)
+buzzer = Buzzer(rpi, 24)
+pir = Pir(rpi, 14)
+
+if button.pressed;
+    led.green_on()
+
